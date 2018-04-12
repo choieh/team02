fef13ff6dfdefdef043e8af3c071c2688d4d014d
hyunjo7o5 <nhj19111@gmail.com>
2018-04-12 17:43:52 +0900

    ㅎㅈ
    ㅎㅈ

--- nam/js/custom.js
+++ nam/js/custom.js
@@ -17,23 +17,23 @@ $(document).ready(function(){
     if(scroll >= height*0 && scroll < height*1){
       $(".fa").css("color", "#fff");
       $(".nav_list>li:nth-child(1)").find(".ani-txt").addClass("on");
-      $(".nav_list>li:nth-child(1)").find(".change").css("color", "red");
+      $(".nav_list>li:nth-child(1)").find(".change").css("color", "purple");
     } else if(scroll >= height*1 && scroll < height*2) {
       $(".fa").css("color", "#fff");
       $(".nav_list>li:nth-child(2)").find(".ani-txt").addClass("on");
-      $(".nav_list>li:nth-child(2)").find(".change").css("color", "red");
+      $(".nav_list>li:nth-child(2)").find(".change").css("color", "purple");
     } else if(scroll >= height*2 && scroll < height*3){
       $(".fa").css("color", "#fff");
       $(".nav_list>li:nth-child(3)").find(".ani-txt").addClass("on");
-      $(".nav_list>li:nth-child(3)").find(".change").css("color", "red");
+      $(".nav_list>li:nth-child(3)").find(".change").css("color", "purple");
     } else if(scroll >= height*3 && scroll < height*4){
       $(".fa").css("color", "#fff");
       $(".nav_list>li:nth-child(4)").find(".ani-txt").addClass("on");
-      $(".nav_list>li:nth-child(4)").find(".change").css("color", "red");
+      $(".nav_list>li:nth-child(4)").find(".change").css("color", "purple");
     } else if(scroll >= height*4 && scroll < height*5){
       $(".fa").css("color", "#fff");
       $(".nav_list>li:nth-child(5)").find(".ani-txt").addClass("on");
-      $(".nav_list>li:nth-child(5)").find(".change").css("color", "red");
+      $(".nav_list>li:nth-child(5)").find(".change").css("color", "purple");
     }
   });
 
@@ -45,19 +45,19 @@ $(document).ready(function(){
     $('html, body').stop().animate({"scrollTop":nowTop},1000);
     if(list==0){
       $(".change").css("color", "#fff");
-      $(".nav_list>li:nth-child(1)").find(".change").css("color", "red");
+      $(".nav_list>li:nth-child(1)").find(".change").css("color", "purple");
     } else if(list==1) {
       $(".fa").css("color", "#fff");
-      $(".nav_list>li:nth-child(2)").find(".change").css("color", "red");
+      $(".nav_list>li:nth-child(2)").find(".change").css("color", "purple");
     } else if(list==2){
       $(".fa").css("color", "#fff");
-      $(".nav_list>li:nth-child(3)").find(".change").css("color", "red");
+      $(".nav_list>li:nth-child(3)").find(".change").css("color", "purple");
     } else if(list==3){
       $(".fa").css("color", "#fff");
-      $(".nav_list>li:nth-child(4)").find(".change").css("color", "red");
+      $(".nav_list>li:nth-child(4)").find(".change").css("color", "purple");
     } else if(list==4){
       $(".fa").css("color", "#fff");
-      $(".nav_list>li:nth-child(5)").find(".change").css("color", "red");
+      $(".nav_list>li:nth-child(5)").find(".change").css("color", "purple");
     }
 
   });
